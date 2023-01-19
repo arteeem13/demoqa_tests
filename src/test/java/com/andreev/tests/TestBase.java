8b5d8b7bf31c869fc7a41b5cfe60e43201180bfc
qa-artem <artem1305@yandex.ru>
2023-01-19 18:47:18 +0300

    added attachments in tests

--- src/test/java/com/andreev/tests/TestBase.java
+++ src/test/java/com/andreev/tests/TestBase.java
@@ -1,8 +1,12 @@
 package com.andreev.tests;
 
+import com.andreev.helpers.Attachments;
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.ArrayList;
 
 public class TestBase {
 
@@ -15,5 +19,13 @@ public class TestBase {
             Configuration.browserSize = "1920x1080";
             Configuration.baseUrl = "https://demoqa.com";
             Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    public static void afterEachTests() {
+        Attachments.screenshotAs("Screenshot");
+        Attachments.pageSource();
+        Attachments.browserConsoleLogs();
+        Attachments.addVideo();
     }
 }
